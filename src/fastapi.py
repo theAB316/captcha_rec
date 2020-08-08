9e6b3ad10f678207df1ab95ca24020964e5c915a
Aneesh Bhat <imaneeshbhat@gmail.com>
2020-08-08 11:14:05 +0530

    FastAPI get image API not working.

--- src/fastapi.py
+++ src/fastapi.py
@@ -1,17 +1,17 @@
 import numpy as np
 import torch
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-from fastapi import FastAPI, File, UploadFile
 
 import config
 from model import CaptchaModel
 from train import decode_predictions
 
+# Create FastAPI app
 app = FastAPI()
 
+# Enable CORS
 origins = ["*"]
 
 app.add_middleware(
@@ -22,13 +22,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# class Item(BaseModel):
-#     image: bytes
-
 @app.get("/")
 def read_main():
     return {"welcome": "welcome"}
 
-@app.post("/")
-def create_upload_file(file: UploadFile = File(...)):
-    return {"filename": file.filename}
+@app.post("/predict")
+def create_upload_file(file: bytes = File(...)):
+    return {"file size": len(file)}
